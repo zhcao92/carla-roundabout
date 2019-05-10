b601f9bf186a9a1026b26ab0da59569e2cb7f5d0
zhcao <caoc11@foxmail.com>
2019-05-09 20:28:09 -0400

    Initial

---
+++ PythonAPI/agents/tools/clock.py
@@ -0,0 +1,23 @@
+import time
+
+class WorldClock:
+    def __init__(self, world):
+        self.time = 0
+        self.lasttime = None
+        world.on_tick(self.tick)
+
+    def tick(self, timestamp):
+        self.time = timestamp.elapsed_seconds
+        #print(self.time)
+
+    def dt(self, wait_for_zero=True):
+        #print(self.time, self.lasttime, time.time())
+        if self.lasttime == None:
+            self.lasttime = self.time
+            return 0.05
+        else:
+            ret = 0
+            while ret == 0 and wait_for_zero:
+                ret = self.time - self.lasttime
+            self.lasttime = self.time
+            return ret
